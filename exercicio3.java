84571602862a201441acc3bae1f0a461d1a6d94b
Caique Ramos <129698408+caique0299@users.noreply.github.com>
2023-04-18 17:25:44 -0300

    Update exercicio3.java

--- exercicio3.java
+++ exercicio3.java
@@ -11,7 +11,7 @@ public class exercicio3 {
 
         Scanner entrada = new Scanner(System.in);
 
-        // Preenchendo a matriz com os valores fornecidos pelo usuário
+        
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print("Digite o valor da posição [" + i + "][" + j + "]: ");
@@ -19,24 +19,24 @@ public class exercicio3 {
             }
         }
 
-        // Exibindo todos os elementos da diagonal principal
+        
         System.out.print("\nElementos da Diagonal Principal:\n");
         for (int i = 0; i < 3; i++) {
             System.out.print(matriz[i][i] + " ");
             somaDiagonalPrincipal += matriz[i][i];
         }
 
-        // Exibindo todos os elementos da diagonal secundária
+        
         System.out.print("\n\nElementos da Diagonal Secundária:\n");
         for (int i = 0; i < 3; i++) {
             System.out.print(matriz[i][2 - i] + " ");
             somaDiagonalSecundaria += matriz[i][2 - i];
         }
 
-        // Exibindo a soma dos elementos da diagonal principal
+        
         System.out.print("\n\nSoma dos Elementos da Diagonal Principal:\n" + somaDiagonalPrincipal);
 
-        // Exibindo a soma dos elementos da diagonal secundária
+       
         System.out.print("\n\nSoma dos Elementos da Diagonal Secundária:\n" + somaDiagonalSecundaria);
     }
 }
