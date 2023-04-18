5fbe39cde3519fe0c410c1dd7ce69351f9fad3a0
Caique Ramos <129698408+caique0299@users.noreply.github.com>
2023-04-18 17:27:36 -0300

    Update exercicio2.java

--- exercicio2.java
+++ exercicio2.java
@@ -8,34 +8,34 @@ public class exercicio2 {
 
 	Scanner entrada = new Scanner(System.in);
 
-    int[] vetor = new int[10]; // vetor com os números
-    int soma = 0, numPares = 0; // variáveis para armazenar a soma dos números e a quantidade de números pares
-    double media = 0.0; // variável para armazenar a média dos números
+    int[] vetor = new int[10]; 
+    int soma = 0, numPares = 0; 
+    double media = 0.0; 
 
-    // lê os números digitados pelo usuário e armazena no vetor
+    
     for (int i = 0; i < vetor.length; i++) {
         System.out.print("Digite o " + (i+1) + "º número: ");
         vetor[i] = entrada.nextInt();
 
-        soma += vetor[i]; // soma os números
+        soma += vetor[i]; 
         if (vetor[i] % 2 == 0) {
-            numPares++; // conta a quantidade de números pares
+            numPares++; 
         }
     }
 
     System.out.println("\nElementos nos índices ímpares:\n");
     for (int i = 1; i < vetor.length; i += 2) {
-        System.out.print(vetor[i] + " "); // exibe os elementos nos índices ímpares
+        System.out.print(vetor[i] + " "); 
     }
 
     System.out.println("\n\nElementos pares:\n");
     for (int i = 0; i < vetor.length; i++) {
         if (vetor[i] % 2 == 0) {
-            System.out.print(vetor[i] + " "); // exibe os elementos pares
+            System.out.print(vetor[i] + " "); 
         }
     }
 
-    media = (double) soma / vetor.length; // calcula a média
+    media = (double) soma / vetor.length; 
     System.out.println("\n\nSoma: " + soma);
     System.out.printf("\nMédia: %.2f", media);
 
